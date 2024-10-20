44d7623ee7bbd08ff24490e2e4b25026f24b7cfd
cdog0666 <cdog0666@outlook.com>
2024-10-20 11:11:11 -0700

    DevLog #1: Setup Up Bubble Sort Screen And Array Drawing

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
 import pygame
 import random
 
+
 #Array Setup
 array1 = []
 for x in range(0,100):
     array1.append(random.randint(1,100))
 array2 = array1
 
+#SortingFunctions
+def bubblesort(array):
+    for i in range(len(array)):
+        for j in range(len(array)-1, i, -1):
+            if array[j]<array[j-1]:
+                array[j], array[j-1] = array[j-1], array[j]
+    return array
+
+
+
 # pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+FPS = 60
+WIDTH,HEIGHT = 1280,720
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Algo Race")
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def draw_array(array,window, color):
+    y=0
+    for x in array:
+        pygame.draw.rect(window,color,pygame.Rect(y,HEIGHT-x,WIDTH/100,x))
+        y=y+(WIDTH/100)
 
-    
-    screen.fill("black")
+#Main
+def draw_window():
+    WIN.fill("BLACK")
+    draw_array(array1,WIN,"blue")
+    pygame.display.update()
 
-    # RENDER YOUR GAME HERE
-    
-  
-    pygame.display.flip()
-    clock.tick(60)
-pygame.quit()
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+    pygame.quit()
+
+#Run
+if __name__=="__main__":
+    main()
+
+
+
